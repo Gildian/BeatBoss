1451215a50c8ffa7f563ecfbccec4a2a73fb4ea2
Gildian Gonzales <gildiangonzales@gmail.com>
2024-03-24 15:50:43 -0400

    first commit

---
+++ backend/beatboss/beatboss/src/main/java/com/beatboss/beatboss/BeatbossApplication.java
@@ -0,0 +1,13 @@
+package com.beatboss.beatboss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeatbossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeatbossApplication.class, args);
+	}
+
+}
